be9541cf448efef6b30a17e48fa97706538496c5
Frederik Hjorslev <frederik@hjorslev.com>
2019-07-17 16:13:27 +0200

    require admin in Install-SteamCMD

--- SteamPS/Public/Install-SteamCMD.ps1
+++ SteamPS/Public/Install-SteamCMD.ps1
@@ -1,3 +1,4 @@
+#Requires -RunAsAdministrator
 function Install-SteamCMD {
     <#
     .SYNOPSIS
--- SteamPS/SteamPS.psm1
+++ SteamPS/SteamPS.psm1
@@ -1,10 +1,8 @@
-﻿#Requires -RunAsAdministrator
-
-#Get public and private function definition files.
+﻿# Get public and private function definition files.
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -ErrorAction SilentlyContinue )
 
-#Dot source the files
+# Dot source the files
 foreach ($Import in @($Public + $Private)) {
     try {
         . $Import.FullName
@@ -14,3 +12,8 @@ foreach ($Import in @($Public + $Private)) {
 }
 
 Export-ModuleMember -Function $Public.BaseName
+
+if ($Host.Name -eq 'Windows PowerShell ISE Host') {
+    Write-Warning -Message 'Output from SteamCMD might not be visible in PowerShell ISE, but will run in the background.'
+    Write-Warning -Message 'Consider using PowerShell ConsoleHost instead.'
+}
