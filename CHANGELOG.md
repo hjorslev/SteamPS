e8a12d7d72e15ecdda9456e27f06d23b329b1222
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2019-07-18 17:00:35 +0200

    test module version in changelog against manifest

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](https://semver.org/).
 
+## [1.2.2] - Unreleased
+
 ## [1.2.1] - 17/02-2019
 
 ### Added
--- Tests/ModuleValidation.Tests.ps1
+++ Tests/ModuleValidation.Tests.ps1
@@ -3,31 +3,33 @@ $ModuleRoot = Split-Path (Resolve-Path "$($ProjectRoot)/*/*.psm1")
 $ModuleName = Split-Path $ModuleRoot -Leaf
 
 $PublicFiles = @(Get-ChildItem -Path "$($ProjectRoot)\Public\*.ps1" -ErrorAction SilentlyContinue)
-$ModuleInformation = Import-Metadata -Path "$($ProjectRoot)\$($ModuleName)\$($ModuleName).psd1"
+$ModuleInformation = Import-Metadata -Path "$($ProjectRoot)\$($ModuleName)\$($ModuleName).psd1" # Cmdlet from module Configuration.
 
 Describe "General project validation: $($ModuleName)" {
-    $FileSearch = @{
-        Path    = $ProjectRoot
-        Include = '*.ps1', '*.psm1', '*.psd1'
-        Recurse = $true
-    }
-    $Scripts = Get-ChildItem @FileSearch
+    Context "Project files" {
+        $FileSearch = @{
+            Path    = $ProjectRoot
+            Include = '*.ps1', '*.psm1', '*.psd1'
+            Recurse = $true
+        }
+        $Scripts = Get-ChildItem @FileSearch
 
-    # TestCases are splatted to the script so we need hashtables
-    $TestCases = $Scripts | ForEach-Object { @{File = $_ } }
-    It "<File> should be valid powershell" -TestCases $TestCases {
-        param($File)
+        # TestCases are splatted to the script so we need hashtables
+        $TestCases = $Scripts | ForEach-Object { @{File = $_ } }
+        It "<File> should be valid powershell" -TestCases $TestCases {
+            param($File)
 
-        $File.FullName | Should -Exist
+            $File.FullName | Should -Exist
 
-        $FileContents = Get-Content -Path $File.FullName -ErrorAction Stop
-        $Errors = $null
-        [System.Management.Automation.PSParser]::Tokenize($FileContents, [ref]$Errors) > $null
-        $Errors.Count | Should -Be 0
-    }
+            $FileContents = Get-Content -Path $File.FullName -ErrorAction Stop
+            $Errors = $null
+            [System.Management.Automation.PSParser]::Tokenize($FileContents, [ref]$Errors) > $null
+            $Errors.Count | Should -Be 0
+        }
 
-    It "'$ModuleName' can import cleanly" {
-        { Import-Module (Join-Path $ModuleRoot "$ModuleName.psm1") -Force } | Should -Not -Throw
+        It "'$ModuleName' can import cleanly" {
+            { Import-Module (Join-Path $ModuleRoot "$ModuleName.psm1") -Force } | Should -Not -Throw
+        }
     }
 
     Context 'Manifest' {
@@ -49,5 +51,12 @@ Describe "General project validation: $($ModuleName)" {
                 $Commandompare.InputObject -join ',' | Should -BeNullOrEmpty
             }
         }
-    } # Context
-}# Descrive
+    } # Context: Manifest
+
+    Context "Changelog" {
+        It "Version in Changelog should not be equal to version in Manifest" {
+            # Expects that the latest version is located at line 8.
+            $ChangeLog = [version]((Get-Content -Path '.\CHANGELOG.md')[7]).Substring(4, 5) | Should -BeGreaterThan (Test-ModuleManifest -Path ".\$($ModuleName)\$($ModuleName).psd1").Version
+        }
+    } # Context: Changelog
+}# Describe
