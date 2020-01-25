db74ed27fa4ce08b0c9e9b7d6e5ea0a6b23f357b
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2020-01-25 17:04:56 +0100

    :art: (Build): Exclude PSScriptAnalyzer rules
    Use PSScriptAnalyzerSettings.psd1 to exclude rules

--- Tests/PSScriptAnalyzer.Tests.ps1
+++ Tests/PSScriptAnalyzer.Tests.ps1
@@ -1,17 +1,14 @@
 ﻿$ScriptAnalyzerRules = Get-ScriptAnalyzerRule
-$PowerShellFiles = Get-ChildItem $env:BHProjectPath -Recurse -Filter *.ps*1 -Exclude 'Use-Module.ps1'
-$ExcludeRules = 'PSAvoidUsingWriteHost', 'PSAvoidUsingInvokeExpression', 'PSShouldProcess'
+$PowerShellFiles = Get-ChildItem $env:BHProjectPath -Recurse -Filter *.ps*1
 
 foreach ($PowerShellFile in $PowerShellFiles) {
     Describe "File $($PowerShellFile) should not produce any PSScriptAnalyzer warnings" {
-        $Analysis = Invoke-ScriptAnalyzer -Path $PowerShellFile.FullName -ExcludeRule $ExcludeRules
+        $Analysis = Invoke-ScriptAnalyzer -Path $PowerShellFile.FullName -Settings "$($env:BHProjectPath)\PSScriptAnalyzerSettings.psd1"
 
         foreach ($Rule in $ScriptAnalyzerRules) {
             It "Should pass $Rule" {
                 if ($Analysis.RuleName -contains $Rule) {
-
-                    $Analysis | Where-Object RuleName -eq $Rule -OutVariable Failures | Out-Default
-
+                    $Analysis | Where-Object -FilterScript { $_.RuleName -eq $Rule } -OutVariable Failures | Out-Default
                     $Failures.Count | Should -Be 0
                 }
             } # It
