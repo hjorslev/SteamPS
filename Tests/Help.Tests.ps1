0875e53415b1987e4c013c6f69bd89bd2cf71f04
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2020-07-07 20:40:52 +0200

    :recycle: Refactor Pester tests

--- Tests/Help.Tests.ps1
+++ Tests/Help.Tests.ps1
@@ -1,66 +1,83 @@
-﻿# https://lazywinadmin.com/2016/05/using-pester-to-test-your-comment-based.html
-# Refactored by Joel Sallow - https://github.com/vexx32/
+﻿# By Joel Sallow - https://github.com/vexx32/
 
 BeforeAll {
     Import-Module $env:BHPSModuleManifest
 }
 
-Describe "$env:BHProjectName Comment Based Help" -Tags "Module" {
+Describe "$env:BHProjectName Sanity Tests - Help Content" -Tags 'Module' {
+
     #region Discovery
+
+    # The module will need to be imported during Discovery since we're using it to generate test cases / Context blocks
     Import-Module $env:BHPSModuleManifest
 
     $ShouldProcessParameters = 'WhatIf', 'Confirm'
 
-    # Generate function test cases
-    $FunctionList = Get-ChildItem -Path "$env:BHModulePath\Public" | ForEach-Object {
-        # The data in these hashtables will map to variables in each It according to the current TestCase (per function)
-        @{
-            Name       = $_.BaseName
-            Path       = $_.FullName
-            Ast        = (Get-Content -Path "function:/$($_.BaseName)").Ast
-            Help       = Get-Help -Name $_.BaseName -Full
-            Parameters = Get-Help $_.BaseName -Parameter * |
-            Where-Object { $_.Name -and $_.Name -notin $ShouldProcessParameters }
-        }
-    }
+    # Generate command list for generating Context / TestCases
+    $Module = Get-Module -Name $env:BHProjectName
+    $CommandList = @(
+        $Module.ExportedFunctions.Keys
+        $Module.ExportedCmdlets.Keys
+    )
+
     #endregion Discovery
 
-    It "has help content for <Name>" -TestCases $FunctionList {
-        $Help | Should -Not -BeNullOrEmpty
-    }
+    foreach ($Command in $CommandList) {
+        Context "$Command - Help Content" {
 
-    It "contains a synopsis for <Name>" -TestCases $FunctionList {
-        $Help.Synopsis | Should -Not -BeNullOrEmpty
-    }
+            #region Discovery
 
-    It "contains a description for <Name>" -TestCases $FunctionList {
-        $Help.Description | Should -Not -BeNullOrEmpty
-    }
+            $Help = @{ Help = Get-Help -Name $Command -Full }
+            $Parameters = Get-Help -Name $Command -Parameter * |
+            Where-Object { $_.Name -and $_.Name -notin $ShouldProcessParameters } |
+            ForEach-Object {
+                @{
+                    Name        = $_.name
+                    Description = $_.Description.Text
+                }
+            }
+            $Ast = @{
+                # Ast will be $null if the command is a compiled cmdlet
+                Ast        = (Get-Content -Path "function:/$Command" -ErrorAction Ignore).Ast
+                Parameters = $Parameters
+            }
+            $Examples = $Help.Help.Examples.Example | ForEach-Object { @{ Example = $_ } }
 
-    It "lists the function author in the Notes section for <Name>" -TestCases $FunctionList {
-        $Notes = $Help.AlertSet.Alert.Text -split '\n'
-        $Notes[0].Trim() | Should -BeLike "Author: *"
-    }
+            #endregion Discovery
 
-    It "has a help entry for each parameter of <Name>" -TestCases $FunctionList {
-        ($Parameters | Measure-Object).Count | Should -Be $Ast.Body.ParamBlock.Parameters.Count -Because 'the number of parameters in the help should match the number in the function script'
-    }
+            It "has help content for $Command" -TestCases $Help {
+                $Help | Should -Not -BeNullOrEmpty
+            }
 
-    It "has a description for all parameters of <Name>" -TestCases $FunctionList {
-        # Candidate for using ShouldActionPreference = Continue
-        foreach ($param in $Parameters) {
-            $param.Description | Should -Not -BeNullOrEmpty -Because "parameter $($param.Name) should have a description!"
-        }
-    }
+            It "contains a synopsis for $Command" -TestCases $Help {
+                $Help.Synopsis | Should -Not -BeNullOrEmpty
+            }
 
-    It "has at least one usage example for <Name>" -TestCases $FunctionList {
-        $Help.Examples.Example.Code.Count | Should -BeGreaterOrEqual 1
-    }
+            It "contains a description for $Command" -TestCases $Help {
+                $Help.Description | Should -Not -BeNullOrEmpty
+            }
 
-    It "lists a description for all examples" -TestCases $FunctionList {
-        # Candidate for using ShouldActionPreference = Continue
-        foreach ($Example in $Help.Examples.Example) {
-            $Example.Remarks | Should -Not -BeNullOrEmpty -Because "example $($Example.Title) should have a description!"
+            It "lists the function author in the Notes section for $Command" -TestCases $Help {
+                $Notes = $Help.AlertSet.Alert.Text -split '\n'
+                $Notes[0].Trim() | Should -BeLike "Author: *"
+            }
+
+            # This will be skipped for compiled commands ($Ast.Ast will be $null)
+            It "has a help entry for all parameters of $Command" -TestCases $Ast -Skip:(-not $Ast.Ast) {
+                ($Parameters | Measure-Object).Count | Should -Be $Ast.Body.ParamBlock.Parameters.Count -Because 'the number of parameters in the help should match the number in the function script'
+            }
+
+            It "has a description for $Command parameter -<Name>" -TestCases $Parameters {
+                $Description | Should -Not -BeNullOrEmpty -Because "parameter $Name should have a description"
+            }
+
+            It "has at least one usage example for $Command" -TestCases $Help {
+                $Help.Examples.Example.Code.Count | Should -BeGreaterOrEqual 1
+            }
+
+            It "lists a description for $Command example: <Title>" -TestCases $Examples {
+                $Example.Remarks | Should -Not -BeNullOrEmpty -Because "example $($Example.Title) should have a description!"
+            }
         }
     }
 } # Describe
