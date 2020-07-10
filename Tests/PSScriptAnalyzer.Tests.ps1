05a8ac9d46c067f590aec22b0f3fb9237d88e023
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2020-07-10 22:58:16 +0200

    :white_check_mark: Add PSScriptAnalyzer tests

---
+++ Tests/PSScriptAnalyzer.Tests.ps1
@@ -0,0 +1,19 @@
+﻿﻿Describe 'PSScriptAnalyzer analysis' {
+    #region Discovery
+    $PowerShellFiles = Get-ChildItem -Path $env:BHModulePath -Recurse -Filter "*.ps*1"
+    #endregion Discovery
+
+    It "<Path> Should not violate: <IncludeRule>" -TestCases @(
+        foreach ($File in $PowerShellFiles) {
+            foreach ($Rule in (Get-ScriptAnalyzerRule)) {
+                @{
+                    IncludeRule = $Rule.RuleName
+                    Path        = $File.FullName
+                }
+            }
+        }
+    ) {
+        param($IncludeRule, $Path)
+        Invoke-ScriptAnalyzer -Path $Path -IncludeRule $IncludeRule -Settings "$env:BHProjectPath\PSScriptAnalyzerSettings.psd1" | Should -BeNullOrEmpty
+    }
+}
