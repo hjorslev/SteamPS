d980d5253a9e375d06ca7458f452f4a9244ff81d
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2020-07-11 19:04:41 +0200

    :white_check_mark: Fix PSScriptAnalyzer tests

--- Tests/PSScriptAnalyzer.Tests.ps1
+++ Tests/PSScriptAnalyzer.Tests.ps1
@@ -1,4 +1,4 @@
-﻿﻿Describe 'PSScriptAnalyzer analysis' {
+﻿Describe 'PSScriptAnalyzer analysis' {
     #region Discovery
     $PowerShellFiles = Get-ChildItem -Path $env:BHModulePath -Recurse -Filter "*.ps*1"
     #endregion Discovery
