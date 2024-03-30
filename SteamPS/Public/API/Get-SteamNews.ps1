4834ed625aa72e680f1982cee393d914512f52e0
Frederik Hjorslev <frederik@hjorslev.com>
2024-03-30 20:09:34 +0100

    refactor cmdlet + test cases

--- SteamPS/Public/API/Get-SteamNews.ps1
+++ SteamPS/Public/API/Get-SteamNews.ps1
@@ -4,47 +4,52 @@
     Returns the latest news of a game specified by its AppID.
 
     .DESCRIPTION
-    Returns the latest news of a game specified by its AppID.
+    Returns the latest news of a game specified by its AppID from the Steam API.
 
     .PARAMETER AppID
-    AppID of the game you want the news of.
+    AppID of the game for which you want to retrieve news.
 
     .PARAMETER Count
-    How many news entries you want to get returned.
+    Specifies the number of news entries to retrieve. Defaults to retrieving all news entries.
 
     .PARAMETER MaxLength
-    Maximum length of each news entry.
-
-    .PARAMETER OutputFormat
-    Format of the output. Options are json (default), xml or vdf.
+    Specifies the maximum length of each news entry. Longer entries will be truncated. Defaults to no truncation.
 
     .EXAMPLE
     Get-SteamNews -AppID 440
 
-    Lists number of news that are available for the AppID.
+    Lists all available news entries for the game with AppID 440.
 
     .EXAMPLE
     Get-SteamNews -AppID 440 -Count 1
 
-    Retrieves 1 (the latest) news item for the AppID 440.
+    Retrieves the latest news entry for the game with AppID 440.
+
+    .EXAMPLE
+    Get-SteamNews -AppID 440 -MaxLength 100
+
+    Lists all available news entries for the game with AppID 440 and truncates
+    the news content to 100 characters.
 
     .INPUTS
-    int64
+    System.Int32
 
     .OUTPUTS
-    Returns a string that is either formatted as json, xml or vdf.
-
-    An appnews object containing:
-
-    appid, the AppID of the game you want news of
-
-    newsitems, an array of news item information:
-    - An ID, title and url.
-    - A shortened excerpt of the contents (to maxlength characters), terminated by "..." if longer than maxlength.
-    - A comma-separated string of labels and UNIX timestamp.
+    An object containing:
+    - GID: The ID of the news item.
+    - Title: The title of the news item.
+    - Url: The URL of the news item.
+    - IsExternalUrl: Indicates if the URL is external.
+    - Author: The author of the news item.
+    - Contents: The content of the news item.
+    - FeedLabel: The label of the news feed.
+    - Date: The date and time when the news item was published.
+    - FeedName: The name of the news feed.
+    - FeedType: The type of the news feed.
+    - AppID: The AppID of the game associated with the news item.
 
     .NOTES
-    Author: Frederik Hjorslelv Nylander
+    Author: Frederik Hjorslev Nylander
 
     .LINK
     https://hjorslev.github.io/SteamPS/Get-SteamNews.html
@@ -53,31 +58,63 @@
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true,
-            HelpMessage = 'AppID of the game you want the news of.')]
+            HelpMessage = 'Specifies the AppID of the game for which you want to retrieve news.')]
         [int]$AppID,
 
         [Parameter(Mandatory = $false,
-            HelpMessage = 'How many news entries you want to get returned.')]
+            HelpMessage = 'Specifies the number of news entries to retrieve.')]
         [int]$Count,
 
         [Parameter(Mandatory = $false,
-            HelpMessage = 'Maximum length of each news entry.')]
-        [int]$MaxLength,
-
-        [Parameter(Mandatory = $false,
-            HelpMessage = 'Format of the output. Options are json (default), xml or vdf.')]
-        [ValidateSet('json', 'xml', 'vdf')]
-        [string]$OutputFormat = 'json'
+            HelpMessage = 'Specifies the maximum length of each news entry.')]
+        [int]$MaxLength
     )
+
 
     begin {
         Write-Verbose -Message "[BEGIN  ] Starting: $($MyInvocation.MyCommand)"
     }
 
     process {
-        $Request = Invoke-WebRequest -Uri "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=$AppID&count=$Count&maxlength=$MaxLength&format=$OutputFormat" -UseBasicParsing
+        # TODO: When only supporting pwsh use null coalescing operator (??)
+        # to handle the case when $Count or $MaxLength is not defined
+        $Body = @{
+            appid = $AppID
+        }
+        if ($Count) {
+            $Body.Add('count', $Count)
+        }
+        if ($MaxLength) {
+            $Body.Add('maxlength', $MaxLength)
+        }
+        $Request = Invoke-RestMethod -Uri 'http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/' -UseBasicParsing -Body $Body
 
-        Write-Output -InputObject $Request.Content
+        if ($Request) {
+            foreach ($Item in $Request.appnews.newsitems) {
+                [PSCustomObject]@{
+                    GID           = [int64]$Item.gid
+                    Title         = $Item.title
+                    Url           = $Item.url
+                    IsExternalUrl = [bool]$Item.is_external_url
+                    Author        = $Item.author
+                    Contents      = $Item.contents
+                    FeedLabel     = $Item.feedlabel
+                    Date          = ((Get-Date "01.01.1970") + ([System.TimeSpan]::FromSeconds($Item.date))).ToString("yyyy-MM-dd HH:mm:ss")
+                    FeedName      = $Item.feedname
+                    FeedType      = $Item.feed_type
+                    AppID         = $Item.appid
+                }
+            }
+        } elseif ($null -eq $Request) {
+            $Exception = [Exception]::new("No news found for $AppID.")
+            $ErrorRecord = [System.Management.Automation.ErrorRecord]::new(
+                $Exception,
+                'NoNewsFound',
+                [System.Management.Automation.ErrorCategory]::ObjectNotFound,
+                $Request
+            )
+            $PSCmdlet.WriteError($ErrorRecord)
+        }
     } # Process
 
     end {
---
+++ Tests/Unit/Public/Get-SteamNews.Tests.ps1
@@ -0,0 +1,58 @@
+﻿Describe "Get-SteamNews" {
+    Context "When providing valid input" {
+        It "Should return news items for a given AppID" {
+            # Mocking Invoke-RestMethod
+            Mock Invoke-RestMethod {
+                $response = '{
+                    "appnews": {
+                        "newsitems": [
+                            {
+                                "gid": 123,
+                                "title": "Test News",
+                                "url": "https://example.com",
+                                "is_external_url": false,
+                                "author": "Test Author",
+                                "contents": "This is a test news content.",
+                                "feedlabel": "Test Feed",
+                                "date": "1706900040",
+                                "feedname": "Test Feed",
+                                "feed_type": "Test",
+                                "appid": 440
+                            }
+                        ]
+                    }
+                }'
+                return $response | ConvertFrom-Json
+            }
+
+            # Call the function
+            $result = Get-SteamNews -AppID 440
+
+            # Assertions
+            $result.Count | Should -Be 1
+            $result[0].GID | Should -Be 123
+            $result[0].Title | Should -Be "Test News"
+            $result[0].Url | Should -Be "https://example.com"
+            $result[0].IsExternalUrl | Should -Be $false
+            $result[0].Author | Should -Be "Test Author"
+            $result[0].Contents | Should -Be "This is a test news content."
+            $result[0].FeedLabel | Should -Be "Test Feed"
+            $result[0].Date | Should -Be "2024-02-02 18:54:00"
+            $result[0].FeedName | Should -Be "Test Feed"
+            $result[0].FeedType | Should -Be "Test"
+            $result[0].AppID | Should -Be 440
+        }
+    }
+
+    Context "When no news found for the given AppID" {
+        It "Should throw an error" {
+            # Mocking Invoke-RestMethod to return null
+            Mock Invoke-RestMethod {
+                return $null
+            }
+
+            # Call the function
+            { Get-SteamNews -AppID 123 -ErrorAction Stop } | Should -Throw "No news found for 123."
+        }
+    }
+}
