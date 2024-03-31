97c0f9d54b430e619e435bcf0f693afc67213e67
Frederik Hjorslev <frederik@hjorslev.com>
2024-03-31 21:24:19 +0200

    add test case for Count parameter

--- Tests/Unit/Public/Get-SteamNews.Tests.ps1
+++ Tests/Unit/Public/Get-SteamNews.Tests.ps1
@@ -1,4 +1,4 @@
-﻿Describe "Get-SteamNews Tests" {
+﻿Describe "Get-SteamNews Tests" -Tag "Unit" {
     Context "When providing valid input" {
         BeforeAll {
             Mock -CommandName Invoke-RestMethod -ModuleName SteamPS -MockWith {
@@ -11,13 +11,26 @@
                                 "url": "https://example.com",
                                 "is_external_url": false,
                                 "author": "Test Author",
-                                "contents": "This is a test news content.",
+                                "contents": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt justo feugiat, pulvinar mi eu, lacinia turpis.",
                                 "feedlabel": "Test Feed",
                                 "date": "1706900040",
                                 "feedname": "Test Feed",
                                 "feed_type": "Test",
                                 "appid": 440
-                            }
+                            },
+                            {
+                                "gid": 456,
+                                "title": "Test 2",
+                                "url": "https://test.com",
+                                "is_external_url": true,
+                                "author": "Test Author",
+                                "contents": "This is a test news content.",
+                                "feedlabel": "Test Feed",
+                                "date": "1706900040",
+                                "feedname": "Feed Test",
+                                "feed_type": "Test",
+                                "appid": 440
+                            },
                         ]
                     }
                 }' | ConvertFrom-Json
@@ -31,12 +44,15 @@
             $SteamNews[0].Url | Should -Be "https://example.com"
             $SteamNews[0].IsExternalUrl | Should -Be $false
             $SteamNews[0].Author | Should -Be "Test Author"
-            $SteamNews[0].Contents | Should -Be "This is a test news content."
+            $SteamNews[0].Contents | Should -Be "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt justo feugiat, pulvinar mi eu, lacinia turpis."
             $SteamNews[0].FeedLabel | Should -Be "Test Feed"
             $SteamNews[0].Date | Should -Be "2024-02-02 18:54:00"
             $SteamNews[0].FeedName | Should -Be "Test Feed"
             $SteamNews[0].FeedType | Should -Be "Test"
             $SteamNews[0].AppID | Should -Be 440
+        }
+        It "Should return 2 news item for a given appID." {
+            (Get-SteamNews -AppID 440 -Count 2 | Measure-Object).Count | Should -BeExactly 2
         }
     }
 
