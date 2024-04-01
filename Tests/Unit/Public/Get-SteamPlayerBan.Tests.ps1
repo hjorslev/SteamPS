ff9183a734b8743e82986dfa58f108ad0a42b9ee
Frederik Hjorslev <frederik@hjorslev.com>
2024-04-01 21:47:10 +0200

    typo

--- Tests/Unit/Public/Get-SteamPlayerBan.Tests.ps1
+++ Tests/Unit/Public/Get-SteamPlayerBan.Tests.ps1
@@ -1,4 +1,4 @@
-﻿Describe "Get-SteamPlayerBan" {
+﻿Describe "Get-SteamPlayerBan Tests" {
     BeforeAll {
         . $SteamPSModulePath\Private\API\Get-SteamAPIKey.ps1
         Mock -CommandName Get-SteamAPIKey -ModuleName SteamPS -MockWith {
