977dcf4365e2065f9836e6971e51d019a8c022da
Frederik Hjorslev <frederik@hjorslev.com>
2024-04-02 21:52:49 +0200

    single quotes

--- Tests/Unit/Public/Get-SteamNews.Tests.ps1
+++ Tests/Unit/Public/Get-SteamNews.Tests.ps1
@@ -1,5 +1,5 @@
-﻿Describe "Get-SteamNews Tests" -Tag "Unit" {
-    Context "When providing valid input" {
+﻿Describe 'Get-SteamNews Tests' -Tag 'Unit' {
+    Context 'When providing valid input' {
         BeforeAll {
             Mock -CommandName Invoke-RestMethod -ModuleName SteamPS -MockWith {
                 return '{
@@ -36,34 +36,34 @@
                 }' | ConvertFrom-Json
             }
         }
-        It "Should return news items for a given AppID" {
+        It 'Should return news items for a given AppID' {
             $SteamNews = Get-SteamNews -AppID 440
 
             $SteamNews[0].GID | Should -Be 123
-            $SteamNews[0].Title | Should -Be "Test News"
-            $SteamNews[0].Url | Should -Be "https://example.com"
+            $SteamNews[0].Title | Should -Be 'Test News'
+            $SteamNews[0].Url | Should -Be 'https://example.com'
             $SteamNews[0].IsExternalUrl | Should -Be $false
-            $SteamNews[0].Author | Should -Be "Test Author"
-            $SteamNews[0].Contents | Should -Be "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt justo feugiat, pulvinar mi eu, lacinia turpis."
-            $SteamNews[0].FeedLabel | Should -Be "Test Feed"
-            $SteamNews[0].Date | Should -Be "2024-02-02 18:54:00"
-            $SteamNews[0].FeedName | Should -Be "Test Feed"
-            $SteamNews[0].FeedType | Should -Be "Test"
+            $SteamNews[0].Author | Should -Be 'Test Author'
+            $SteamNews[0].Contents | Should -Be 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt justo feugiat, pulvinar mi eu, lacinia turpis.'
+            $SteamNews[0].FeedLabel | Should -Be 'Test Feed'
+            $SteamNews[0].Date | Should -Be '2024-02-02 18:54:00'
+            $SteamNews[0].FeedName | Should -Be 'Test Feed'
+            $SteamNews[0].FeedType | Should -Be 'Test'
             $SteamNews[0].AppID | Should -Be 440
         }
-        It "Should return 2 news item for a given appID." {
+        It 'Should return 2 news item for a given appID.' {
             (Get-SteamNews -AppID 440 -Count 2 | Measure-Object).Count | Should -BeExactly 2
         }
     }
 
-    Context "When no news found for the given AppID" {
+    Context 'When no news found for the given AppID' {
         BeforeAll {
             Mock -CommandName Invoke-RestMethod -ModuleName SteamPS -MockWith {
                 return $null
             }
         }
-        It "Should throw an error" {
-            { Get-SteamNews -AppID 123 -ErrorAction Stop } | Should -Throw "No news found for 123."
+        It 'Should throw an error' {
+            { Get-SteamNews -AppID 123 -ErrorAction Stop } | Should -Throw 'No news found for 123.'
         }
     }
 }
