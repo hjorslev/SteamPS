fde8a703d9dbd62b8ec6f4fd345a9dd493f9624d
Frederik Hjorslev <frederik@hjorslev.com>
2024-04-02 21:55:17 +0200

    single quotes

--- Tests/Unit/Public/Resolve-VanityURL.Tests.ps1
+++ Tests/Unit/Public/Resolve-VanityURL.Tests.ps1
@@ -1,4 +1,4 @@
-﻿Describe "Resolve-VanityURL Tests" -Tag "Unit" {
+﻿Describe 'Resolve-VanityURL Tests' -Tag 'Unit' {
     BeforeAll {
         . $SteamPSModulePath\Private\API\Get-SteamAPIKey.ps1
         Mock -CommandName Get-SteamAPIKey -ModuleName SteamPS -MockWith {
@@ -6,32 +6,32 @@
         }
     }
 
-    Context "When resolving a valid VanityURL" {
+    Context 'When resolving a valid VanityURL' {
         BeforeAll {
             Mock -CommandName Invoke-RestMethod -ModuleName SteamPS -MockWith {
                 return '{"response":{"steamid":"7656119711117235","success": 1}}' | ConvertFrom-Json
             } # Mock
         }
-        It "Should return a PSCustomObject with SteamID64" {
-            (Resolve-VanityURL -VanityURL "Toby").SteamID64 | Should -BeExactly 7656119711117235
+        It 'Should return a PSCustomObject with SteamID64' {
+            (Resolve-VanityURL -VanityURL 'Toby').SteamID64 | Should -BeExactly 7656119711117235
         }
     }
 
-    Context "When resolving an invalid VanityURL" {
+    Context 'When resolving an invalid VanityURL' {
         BeforeAll {
             Mock -CommandName Invoke-RestMethod -ModuleName SteamPS -MockWith {
                 return '{"response":{"success": 42,"message": "No match"}}' | ConvertFrom-Json
             } # Mock
         }
 
-        It "Should throw an error" {
-            { Resolve-VanityURL -VanityURL "invalidVanityURL" -ErrorAction Stop } | Should -Throw
+        It 'Should throw an error' {
+            { Resolve-VanityURL -VanityURL 'invalidVanityURL' -ErrorAction Stop } | Should -Throw
         }
     }
 
-    Context "When resolving an invalid, fully qualified VanityURL" {
-        It "Should throw an error" {
-            { Resolve-VanityURL -VanityURL "https://steamcommunity.com/id/test" } | Should -Throw
+    Context 'When resolving an invalid, fully qualified VanityURL' {
+        It 'Should throw an error' {
+            { Resolve-VanityURL -VanityURL 'https://steamcommunity.com/id/test' } | Should -Throw
         }
     }
 }
