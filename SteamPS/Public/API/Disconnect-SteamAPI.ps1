204aea0448ce30781904c723a062590941640dc7
Frederik Hjorslev <frederik@hjorslev.com>
2024-04-07 00:23:38 +0200

    add cmdlet & add pester test

---
+++ SteamPS/Public/API/Disconnect-SteamAPI.ps1
@@ -0,0 +1,53 @@
+﻿function Disconnect-SteamAPI {
+    <#
+    .SYNOPSIS
+    Disconnects from the Steam API by removing the stored API key.
+
+    .DESCRIPTION
+    The Disconnect-SteamAPI cmdlet removes the stored Steam API key from the system. This effectively disconnects the current session from the Steam API.
+
+    .PARAMETER Force
+    When the Force switch is used, the cmdlet will skip the confirmation prompt and directly remove the API key.
+
+    .EXAMPLE
+    Disconnect-SteamAPI -Force
+
+    This command will remove the stored Steam API key without asking for confirmation.
+
+    .INPUTS
+    None. You cannot pipe objects to Disconnect-SteamAPI.
+
+    .OUTPUTS
+    None. Nothing is returned when calling Disconnect-SteamAPI.
+
+    .NOTES
+    Author: sysgoblin (https://github.com/sysgoblin) and Frederik Hjorslev Nylander
+
+    .LINK
+    https://hjorslev.github.io/SteamPS/Disconnect-SteamAPI.html
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false,
+            HelpMessage = 'Skip the confirmation prompt.')][switch]$Force
+    )
+
+    begin {
+        Write-Verbose -Message "[BEGIN  ] Starting: $($MyInvocation.MyCommand)"
+        $SteamAPIKey = "$env:AppData\SteamPS\SteamPSKey.json"
+    }
+
+    process {
+        if ($Force -or $PSCmdlet.ShouldContinue($SteamAPIKey, 'Do you want to continue removing the API key?')) {
+            if (Test-Path -Path $SteamAPIKey) {
+                Remove-Item -Path $SteamAPIKey -Force
+                Write-Verbose -Message "$SteamAPIKey were deleted."
+            }
+        }
+    }
+
+    end {
+        Write-Verbose -Message "[END    ] Ending: $($MyInvocation.MyCommand)"
+    }
+}
---
+++ Tests/Unit/Public/Disconnect-SteamAPI.Tests.ps1
@@ -0,0 +1,12 @@
+﻿Describe 'Disconnect-SteamAPI' {
+    BeforeAll {
+        Mock -CommandName Test-Path -MockWith { return $true }
+        Mock -CommandName Remove-Item -MockWith {}
+    }
+
+    It 'removes the API key when -Force is used' {
+        Disconnect-SteamAPI -Force
+        Should -Invoke Test-Path -Times 1 -Scope It
+        Should -Invoke Remove-Item -Times 1 -Scope It
+    }
+}
