d59306d5ea9f1012ad3f6c60453b8c27388e6f44
Frederik Hjorslev <frederik@hjorslev.com>
2024-04-07 23:09:05 +0200

    fix tests

--- Tests/Unit/Public/Disconnect-SteamAPI.Tests.ps1
+++ Tests/Unit/Public/Disconnect-SteamAPI.Tests.ps1
@@ -1,29 +1,24 @@
-﻿Describe 'Disconnect-SteamAPI Tests' {
-    Context 'With an API key' {
-        BeforeAll {
-            Mock -CommandName Test-Path -ModuleName SteamPS -MockWith {
-                return $true
-            }
-            Mock -CommandName Remove-Item -ModuleName SteamPS -MockWith {
-                return $true
-            }
-        }
-        It 'Should remove the API key when -Force is used' {
-            { Disconnect-SteamAPI -Force | Should -Not -Throw }
+﻿Describe 'Disconnect-SteamAPI' {
+    Mock -ModuleName SteamPS Write-Verbose {} -Verifiable -ParameterFilter { $Message -eq "[BEGIN  ] Starting: $($MyInvocation.MyCommand)" }
+    Mock -ModuleName SteamPS Test-Path { return $true } -Verifiable -ParameterFilter { $Path -eq "$env:AppData\SteamPS\SteamPSKey.json" }
+    Mock -ModuleName SteamPS Remove-Item {} -Verifiable -ParameterFilter { $Path -eq "$env:AppData\SteamPS\SteamPSKey.json" }
+    Mock -ModuleName SteamPS Write-Verbose {} -Verifiable -ParameterFilter { $Message -eq "$SteamAPIKey were deleted." }
+    Mock -ModuleName SteamPS [Exception]::new {} -Verifiable -ParameterFilter { $Message -eq "Steam Web API configuration file not found in '$env:AppData\SteamPS\SteamPSKey.json'." }
+    Mock -ModuleName SteamPS [System.Management.Automation.ErrorRecord]::new {} -Verifiable -ParameterFilter { $CategoryInfo.Category -eq [System.Management.Automation.ErrorCategory]::ObjectNotFound }
+    Mock -ModuleName SteamPS $PSCmdlet.ThrowTerminatingError {} -Verifiable -ParameterFilter { $ErrorId -eq 'SteamAPIKeyNotFound' }
+
+    Context 'When Force is specified' {
+        It 'Deletes the Steam API key file' {
+            $result = Disconnect-SteamAPI -Force
+            Should -InvokeVerifiable
         }
     }
 
-    Context 'Without an API key' {
-        BeforeAll {
-            Mock -CommandName Test-Path -ModuleName SteamPS -MockWith {
-                return $false
-            }
-            Mock -CommandName Remove-Item -ModuleName SteamPS -MockWith {
-                return $false
-            }
-        }
-        It 'Should fail when no API key is found' {
-            { Disconnect-SteamAPI -Force | Should -Throw }
+    Context 'When Force is not specified' {
+        Mock -ModuleName SteamPS $PSCmdlet.ShouldContinue { return $true }
+        It 'Deletes the Steam API key file' {
+            $result = Disconnect-SteamAPI
+            Should -InvokeVerifiable
         }
     }
 }
