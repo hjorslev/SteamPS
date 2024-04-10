8c9962ea4f27d46ecb8d97727c9106d3101e0f6b
Frederik Hjorslev <frederik@hjorslev.com>
2024-04-10 22:29:25 +0200

    add cmdlet & add pester test

---
+++ SteamPS/Private/API/Test-SteamAPIKey.ps1
@@ -0,0 +1,59 @@
+﻿function Test-SteamAPIKey {
+    <#
+    .SYNOPSIS
+    Tests whether a Steam API key file exists.
+
+    .DESCRIPTION
+    The `Test-SteamAPIKey` cmdlet checks if a Steam API key file exists in the specified path.
+    It returns a boolean value indicating whether the key file is present.
+
+    .PARAMETER None
+    This cmdlet does not accept any parameters.
+
+    .OUTPUTS
+    System.Boolean
+    Returns `$true` if the Steam API key file exists; otherwise, returns `$false`.
+
+    .NOTES
+    File Path: The Steam API key file is expected to be located at "$env:AppData\SteamPS\SteamPSKey.json".
+
+    .EXAMPLE
+    PS C:\> Test-SteamAPIKey
+    True
+
+    Description:
+    This example checks if the Steam API key file exists and returns `True`.
+
+    .EXAMPLE
+    PS C:\> Test-SteamAPIKey
+    False
+
+    Description:
+    This example checks if the Steam API key file exists and returns `False`.
+
+    .NOTES
+    Author: Frederik Hjorslev Nylander
+    #>
+
+    [CmdletBinding()]
+    [OutputType('System.Boolean')]
+    param (
+    )
+
+    begin {
+        Write-Verbose -Message "[BEGIN  ] Starting: $($MyInvocation.MyCommand)"
+        $SteamPSKey = Test-Path -Path "$env:AppData\SteamPS\SteamPSKey.json"
+    }
+
+    process {
+        if ($SteamPSKey -eq $true) {
+            return [bool]$true
+        } elseif ($SteamPSKey -eq $false) {
+            return [bool]$false
+        }
+    }
+
+    end {
+        Write-Verbose -Message "[END    ] Ending: $($MyInvocation.MyCommand)"
+    }
+}
---
+++ Tests/Unit/Private/Test-SteamAPIKey.Tests.ps1
@@ -0,0 +1,23 @@
+﻿Describe 'Test-SteamAPIKey Tests' {
+    Context 'When the Steam API key file exists' {
+        BeforeAll {
+            Mock Test-Path { $true }
+        }
+        It 'Returns $true' {
+            $result = Test-SteamAPIKey
+            $result | Should -BeOfType [bool]
+            $result | Should -Be $true
+        }
+    }
+
+    Context 'When the Steam API key file does not exist' {
+        BeforeAll {
+            Mock Test-Path { $false }
+        }
+        It 'Returns $false' {
+            $result = Test-SteamAPIKey
+            $result | Should -BeOfType [bool]
+            $result | Should -Be $false
+        }
+    }
+}
