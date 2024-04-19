5d349e628c14f5a4a6eb9ffa7179111d37f71f36
Frederik Hjorslev <frederik@hjorslev.com>
2024-04-19 23:31:50 +0200

    comment based help

--- SteamPS/Public/API/Get-SteamPlayerBan.ps1
+++ SteamPS/Public/API/Get-SteamPlayerBan.ps1
@@ -1,16 +1,18 @@
 ﻿function Get-SteamPlayerBan {
     <#
-    .SYNOPSIS
-    Retrieves ban information for Steam players.
+.SYNOPSIS
+    Fetches ban information for Steam players.
 
     .DESCRIPTION
-    This cmdlet retrieves ban information for Steam players, including whether they are banned from the Steam Community, have VAC bans, the number of VAC bans, days since the last ban, number of game bans, and economy ban status.
+    This cmdlet fetches ban information for Steam players. The information includes whether the players are banned from the Steam Community, have VAC bans, the number of VAC bans, days since the last ban, number of game bans, and economy ban status.
 
     .PARAMETER SteamID64
-    Specifies one or more 64-bit Steam IDs for which to retrieve ban information. Enter the Steam IDs as a comma-delimited list.
+    Specifies one or more 64-bit Steam IDs for which to fetch ban information. Enter the Steam IDs as a comma-separated list.
 
     .EXAMPLE
     Get-SteamPlayerBan -SteamID64 76561197960435530,76561197960434622
+
+    This example fetches ban information for the players with the specified Steam IDs.
 
     .INPUTS
     int64[]: Specifies an array of 64-bit integers representing Steam IDs.
@@ -18,13 +20,13 @@
     .OUTPUTS
     Returns objects with the following properties:
 
-    - SteamID64: The player's 64 bit ID.
-    - CommunityBanned: Indicates whether or not the player is banned from Steam Community.
-    - VACBanned: Indicates whether or not the player has VAC bans on record.
-    - NumberOfVACBans: Number of VAC bans on record.
-    - DaysSinceLastBan: Number of days since the last ban.
-    - NumberOfGameBans: Number of bans in games, this includes CS:GO Overwatch bans.
-    - EconomyBan: The player's ban status in the economy. If the player has no bans on record the string will be "none", if the player is on probation it will say "probation", etc.
+    - SteamID64: The player's 64-bit ID.
+    - CommunityBanned: A boolean indicating whether the player is banned from the Steam Community.
+    - VACBanned: A boolean indicating whether the player has VAC bans on record.
+    - NumberOfVACBans: The number of VAC bans on record.
+    - DaysSinceLastBan: The number of days since the last ban.
+    - NumberOfGameBans: The number of bans in games, including CS:GO Overwatch bans.
+    - EconomyBan: The player's ban status in the economy. If the player has no bans on record, the string will be "none". If the player is on probation, it will say "probation", etc.
 
     .NOTES
     Author: Frederik Hjorslev Nylander
