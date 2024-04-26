933556d9cd46289672016edccaa88445a37fddfd
Frederik Hjorslev <frederik@hjorslev.com>
2024-04-26 23:25:47 +0200

    update to support ApplicationID

--- SteamPS/Public/Server/Update-SteamApp.ps1
+++ SteamPS/Public/Server/Update-SteamApp.ps1
@@ -66,9 +66,16 @@ function Update-SteamApp {
         [Parameter(Position = 0,
             Mandatory = $true,
             ValueFromPipelineByPropertyName = $true,
-            ParameterSetName = 'AppID'
+            ParameterSetName = 'ApplicationID'
         )]
-        [int]$AppID,
+        [ValidateScript({
+            if ($null -eq (Get-SteamGame -ApplicationID $_)) {
+                throw "ApplicationID $_ couldn't be found."
+            }
+            $true
+        })]
+        [Alias('AppID')]
+        [int]$ApplicationID,
 
         [Parameter(Mandatory = $true)]
         [ValidateScript( {
@@ -128,7 +135,7 @@ function Update-SteamApp {
         # If game is found by searching for game name.
         if ($PSCmdlet.ParameterSetName -eq 'ApplicationName') {
             try {
-                $SteamApp = Find-SteamAppID -ApplicationName $ApplicationName
+                $SteamApp = Get-SteamGame -ApplicationName $ApplicationName
                 # Install selected Steam application if a SteamAppID has been selected.
                 if (-not ($null -eq $SteamApp)) {
                     if ($Force -or $PSCmdlet.ShouldContinue("Do you want to install or update $(($SteamApp).name)?", "Update SteamApp $(($SteamApp).name)?")) {
@@ -141,10 +148,10 @@ function Update-SteamApp {
             }
         } # ParameterSet ApplicationName
 
-        # If game is found by using a unique AppID.
-        if ($PSCmdlet.ParameterSetName -eq 'AppID') {
+        # If game is found by using a unique ApplicationID.
+        if ($PSCmdlet.ParameterSetName -eq 'ApplicationID') {
             try {
-                $SteamAppID = $AppID
+                $SteamAppID = $ApplicationID
                 # Install selected Steam application.
                 if ($Force -or $PSCmdlet.ShouldContinue("Do you want to install or update $($SteamAppID)?", "Update SteamApp $($SteamAppID)?")) {
                     Write-Verbose -Message "The application with AppID $SteamAppID is being updated. Please wait for SteamCMD to finish."
