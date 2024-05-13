62f8ada6181ab8f3fef3f2108fa2b5894e2b1f3b
Frederik Hjorslev <frederik@hjorslev.com>
2024-05-13 23:32:24 +0200

    Rework Update-SteamServer (#82)
    * test if curent user is admin
    
    * Refactor Pester tests
    
    * refactor & pester tests
    
    * typo
    
    * add end of line

---
+++ Tests/Integration/Public/Install-SteamCMD.Tests.ps1
@@ -0,0 +1,22 @@
+﻿
+Describe 'Install-SteamCMD Tests' -Tag 'Integration' {
+    BeforeAll {
+        . "$($SteamPSModulePath)\Private\Server\Add-EnvPath.ps1"
+        Add-EnvPath -Path 'TestDrive:\Test\SteamCMD'
+    }
+
+    Context 'When executing Install-SteamCMD' {
+        It 'Should install SteamCMD' {
+            Install-SteamCMD -InstallPath 'TestDrive:\Test' -Force
+        }
+
+        It 'Should find steamcmd.exe' {
+            Test-Path -Path "$TestDrive\Test\SteamCMD\steamcmd.exe" | Should -BeTrue
+        }
+    }
+
+    AfterAll {
+        # Wait for the process steamerrorreporter to be closed - else test folder wont be deleted.
+        Wait-Process -Name 'steamerrorreporter' -ErrorAction SilentlyContinue
+    }
+}
---
+++ Tests/Unit/Public/Update-SteamServer.Tests.ps1
@@ -0,0 +1,39 @@
+﻿
+Describe 'Update-SteamServer Tests' -Tag 'Unit' {
+    Context 'When executing Update-SteamServer without SteamCMD installed' {
+        BeforeAll {
+            Mock -CommandName Test-Admin -ModuleName SteamPS -MockWith {
+                return $true
+            }
+            Mock -CommandName Get-SteamPath -ModuleName SteamPS -MockWith {
+                return $null
+            }
+            Mock -CommandName Get-Service -ModuleName SteamPS -MockWith {
+                return @{ Name = 'GB-PG10' }
+            }
+        }
+        It 'Should throw an error' {
+            { Update-SteamServer -AppID 476400 -ServiceName 'GB-PG10' -IPAddress '1.1.1.1' -Port 27015 } | Should -Throw 'SteamCMD could not be found in the env:Path. Have you executed Install-SteamCMD?'
+        }
+    }
+
+    Context 'When executing Update-SteamServer with insufficient permissions' {
+        BeforeAll {
+            Mock -CommandName Test-Admin -ModuleName SteamPS -MockWith {
+                return $false
+            }
+            Mock -CommandName Get-SteamPath -ModuleName SteamPS -MockWith {
+                return [PSCustomObject]@{
+                    'Path'       = 'C:\Program Files\'
+                    'Executable' = 'C:\Program Files\steamcmd.exe'
+                }
+            }
+            Mock -CommandName Get-Service -ModuleName SteamPS -MockWith {
+                return @{ Name = 'GB-PG10' }
+            }
+        }
+        It 'Should throw an error' {
+            { Update-SteamServer -AppID 476400 -ServiceName 'GB-PG10' -IPAddress '1.1.1.1' -Port 27015 } | Should -Throw 'The current PowerShell session is not running as Administrator. Start PowerShell by using the Run as Administrator option, and then try running the script again.'
+        }
+    }
+}
